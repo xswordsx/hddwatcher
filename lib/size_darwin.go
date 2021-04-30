96ce90a076d53dbc7deb0536271e65f8b0f28488
Ivan Latunov <xswordsx@gmail.com>
2021-04-30 18:10:18 +0300

    Set proper return type of GetSize

--- lib/size_darwin.go
+++ lib/size_darwin.go
@@ -3,6 +3,6 @@ package lib
 import "fmt"
 
 // GetSpace returns disk size information for the given drive (bytes).
-func GetSpace(string) (int, int, int, error) {
+func GetSpace(string) (int64, int64, int64, error) {
 	return 0, 0, 0, fmt.Errorf("not implemented")
 }
